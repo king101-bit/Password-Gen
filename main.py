1435af84b45f248c811f4e41063244bdb0411328
Zack <69579338+king101-bit@users.noreply.github.com>
2023-07-14 22:10:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,63 @@
 import secrets
 import string
+import requests
+import hashlib
 
-letters = string.ascii_letters
+def generate_password(length=16, require_special_chars=True, require_digits=True):
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    password = ''
 
-digits = string.digits
+    while True:
+        password = ''.join(secrets.choice(alphabet) for _ in range(length))
 
-special_chars = string.punctuation
+        if (not require_special_chars or any(char in string.punctuation for char in password)) and \
+                (not require_digits or sum(char in string.digits for char in password) >= 3):
+            break
 
-alphabet = letters + digits + special_chars
+    return password
 
-pwd_length = 16
+def is_password_pwned(password):
+    sha1_hash = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    prefix, suffix = sha1_hash[:5], sha1_hash[5:]
+    url = f"https://api.pwnedpasswords.com/range/{prefix}"
+    response = requests.get(url)
+    response_lines = response.text.splitlines()
+    for line in response_lines:
+        if line.startswith(suffix):
+            return True
+    return False
 
-pwd = ''
-for i in range(pwd_length):
-    pwd += ''.join(secrets.choice(alphabet))
+def main_menu():
+    print("===== Password Generator =====")
+    print("1. Generate a new password")
+    print("2. Specify the number of characters")
+    print("3. Check if a password is pwned")
+    print("4. Exit")
+    print("==============================")
 
-if (any(char in special_chars for char in pwd) and
-   sum(char in digits for char in pwd) >= 3):
+    choice = input("Enter your choice (1-4): ")
 
-    print(pwd)
+    if choice == "1":
+        length = int(input("Enter the desired password length (4-20): "))
+        password = generate_password(length=length)
+        print(f"Generated password: {password}")
+    elif choice == "2":
+        num_chars = int(input("Enter the number of characters for the password: "))
+        password = generate_password(length=num_chars)
+        print(f"Generated password: {password}")
+    elif choice == "3":
+        password = input("Enter the password to check: ")
+        if is_password_pwned(password):
+            print("This password has been pwned. Please choose a different one.")
+        else:
+            print("This password has not been pwned. You can use it.")
+    elif choice == "4":
+        return
+    else:
+        print("Invalid choice. Please try again.")
+
+    print()
+    main_menu()
+
+# Start the program
+main_menu()
